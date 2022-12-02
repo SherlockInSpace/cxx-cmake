43de2b497112ccd50953cb6deadec490ca14ebb1
Ryan Sherlock <ryan.m.sherlock@gmail.com>
2022-12-02 05:58:23 -0800

    fix: Remove unnecessary library spdlog
    We are not using spdlog in the libraries, only in the applications.
    
    Signed-off-by: Ryan Sherlock <ryan.m.sherlock@gmail.com>

--- util/src/bloom.cpp
+++ util/src/bloom.cpp
@@ -1,9 +1,5 @@
 #include "bloom.hpp"
 
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
-#include <spdlog/fmt/bin_to_hex.h>
-
 using namespace util;
 
 Bloom::Bloom(size_t hashFunctionCount) :
@@ -24,7 +20,7 @@ Bloom::Bloom(size_t hashFunctionCount) :
             "hashFunctionCount too large! "
             "hashFunctionCount *  kBytesPerHashFunction must be smaller or "
             "equal to kMD5ResultSize");
-    }   
+    }
 }
 
 void Bloom::insert(const std::string& object)
